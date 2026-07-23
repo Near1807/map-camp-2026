8de306d4795f8845b3a48e218fdf56db69b8663d
TheNear <tomtom.leman@gmail.com>
2026-07-23 22:13:27 +0200

    oups

--- app/data/badges.tsx
+++ app/data/badges.tsx
@@ -5,9 +5,9 @@ export type Badge = {
 };
 
 export const badges: Badge[] = [
-  { name: "bug", icon: "bug.png", obtention: true },
+  { name: "bug", icon: "bug.png", obtention: false },
   { name: "dark", icon: "dark.png", obtention: false },
-  { name: "dragon", icon: "dragon.png", obtention: true },
+  { name: "dragon", icon: "dragon.png", obtention: false },
   { name: "electric", icon: "electric.png", obtention: false },
   { name: "fairy", icon: "fairy.png", obtention: false },
   { name: "fighting", icon: "fighting.png", obtention: false },
@@ -15,7 +15,7 @@ export const badges: Badge[] = [
   { name: "flying", icon: "flying.png", obtention: false },
   { name: "ghost", icon: "ghost.png", obtention: false },
   { name: "grass", icon: "grass.png", obtention: false },
-  { name: "ground", icon: "ground.png", obtention: true },
+  { name: "ground", icon: "ground.png", obtention: false },
   { name: "ice", icon: "ice.png", obtention: false },
   { name: "normal", icon: "normal.png", obtention: false },
   { name: "poison", icon: "poison.png", obtention: false },
